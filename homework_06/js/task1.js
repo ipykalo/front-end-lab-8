d02af0ea207098599e02bf68d09235479bbd47d8
Ivan <ipykalo@ukr.net>
2018-02-07 21:54:21 +0200

    homework_06

---
+++ homework_06/js/task1.js
@@ -0,0 +1,56 @@
+var typeTriangle, a, b, c;
+a = +prompt("Enter length first side of triangle", "");
+b = +prompt("Enter length second side of triangle", "");
+c = +prompt("Enter length third side of triangle", "");
+
+// type of Triangle
+
+if (
+	Math.pow(a, 2) == Math.pow(b, 2) + Math.pow(c, 2) ||
+	Math.pow(b, 2) == Math.pow(a, 2) + Math.pow(c, 2) ||
+	Math.pow(c, 2) == Math.pow(a, 2) + Math.pow(b, 2)
+) {
+	typeTriangle = "right triangle";
+} else if (a == b && b == c) {
+	typeTriangle = "equilateral";
+} else if (a == b || b == c || a == c) {
+	typeTriangle = "isosceles";
+} else if (a != b && b != c && a != c) {
+	typeTriangle = "scalene";
+}
+
+(function isNumber(a, b, c) {
+	if (!isNaN(a) && !isNaN(b) && !isNaN(c)) {
+		if (Math.sign(a) == 1 || Math.sign(b) == 1 || Math.sign(c) == 1) {
+			squareTriangle(a, b, c);
+		} else {
+			console.log("Incorrect data");
+		}
+	} else {
+		console.log("Enter only numbers");
+	}
+})(a, b, c);
+
+// square Triangles
+function squareTriangle(a, b, c) {
+	var p = (a * 10 + b * 10 + c * 10) / 10 / 2;
+	var result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+	if (isNaN(result) || result == 0) {
+		console.log("Incorrect data");
+	} else {
+		if (result % 1 != 0) {
+			result = result.toFixed(2);
+
+			if (Number(result)) {
+				console.log(
+					"Type of triangle is " + typeTriangle + " and square is " + result
+				);
+			}
+		} else {
+			console.log(
+				"Type of triangle is " + typeTriangle + " and square is " + result
+			);
+		}
+	}
+}
---
+++ homework_06/js/task2.js
@@ -0,0 +1,45 @@
+var courseEuro = 33.8565;
+var courseUsd = 27.4609;
+
+var amoutEuro = +prompt("Input amount of EURO", "");
+var amoutUsd = +prompt("Input amount of USD", "");
+
+if (!isNaN(amoutEuro) && !isNaN(amoutUsd)) {
+	if (Math.sign(amoutEuro) == 1 && Math.sign(amoutUsd) == 1) {
+		convert(amoutEuro, amoutUsd);
+	} else {
+		console.log("Incorrect data");
+	}
+} else {
+	console.log("Incorrect data");
+}
+
+// Convertering function
+function convert(amoutEuro, amoutUsd) {
+	var euroUah = amoutEuro * courseEuro;
+	euroUah = euroUah.toFixed(2);
+
+	var usdUah = amoutUsd * courseUsd;
+	usdUah = usdUah.toFixed(2);
+
+	var euroUsd = courseEuro / courseUsd;
+	euroUsd = euroUsd.toFixed(2);
+
+	console.log(
+		amoutEuro +
+			" euros are equal " +
+			euroUah +
+			" UAH" +
+			" , " +
+			amoutUsd +
+			" dollars are equal " +
+			usdUah +
+			" UAH" +
+			" , " +
+			"one euro is equal " +
+			euroUsd +
+			" dollars"
+	);
+}
+
+// End convertering function
